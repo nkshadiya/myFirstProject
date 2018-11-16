13c22d4557acaae460afdcbd0f5c88b11eb77758
nksha <nksha@DESKTOP-NUUE1F3>
2018-11-15 21:30:15 -0500

    Added equals test

--- src/main/java/com/example/ImmutableExercise.java
+++ src/main/java/com/example/ImmutableExercise.java
@@ -1,9 +1,5 @@
 package com.example;
 
-/**
- * @author nksha
- *
- */
 public class ImmutableExercise {
 
 	public static void main(String[] args) {
@@ -11,6 +7,14 @@ public class ImmutableExercise {
 		Immutable immutable = null;
 		immutable= new Immutable(1," hi", new Mutable(2));
 		
+		Immutable immutable2;
+		immutable2 = immutable;
+		
+	
 		System.out.println(immutable.equals(new Immutable(1," hi", new Mutable(2))));
+		
+		System.out.println(immutable.equals(immutable2));
+		
+		System.out.println(immutable.equals(new String("")));
 	}
 }
